4290a2161006e98d1700e9024d35aea7106791e8
jasonh <appleoxxo@gmail.com>
2021-01-08 18:01:05 +0800

    add average count

--- monitor.js
+++ monitor.js
@@ -6,11 +6,12 @@ const client = mqtt.connect(mqttUrl);
 console.log(`$ connecting to ${mqttUrl}`)
 
 let count = 0;
+const time = Date.now();
 
 client.on('connect', (connack) => {
   console.log('$ connected');
   setInterval(() => {
-    console.log(new Date(), `message received count ${count}`);
+    console.log(new Date(), `message received count ${count} average ${Math.floor(count/((Date.now()-time)/1000))}/s`);
   }, 1000);
   client.subscribe(`#`, { qos: 1 }, function (err, granted) {
     if (!err) {
